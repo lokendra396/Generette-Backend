c6c779edf24e83f97796c2b52cf8008a08ca12b6
Lokendra Singh <singhlok396@gmail.com>
2023-12-26 23:28:33 +0530

    port change

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const authRoute = require('./routes/auth');
-const resumeRoute = require('./routes/resumeDataHandler');
-const offerLetterRoute = require('./routes/offerLetterDataHandler');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRoute = require("./routes/auth");
+const resumeRoute = require("./routes/resumeDataHandler");
+const offerLetterRoute = require("./routes/offerLetterDataHandler");
 const getRoutes = require("./routes/getRoutes");
-const port = 5000 || process.env.PORT;
-const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 connectDB();
-
-
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/auth', authRoute);
-app.use('/resume', resumeRoute);
-app.use('/offer_letter', offerLetterRoute);
-app.use('/edit', getRoutes);
+app.use("/auth", authRoute);
+app.use("/resume", resumeRoute);
+app.use("/offer_letter", offerLetterRoute);
+app.use("/edit", getRoutes);
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}!`);
-})
+  console.log(`server started on port ${port}!`);
+});
